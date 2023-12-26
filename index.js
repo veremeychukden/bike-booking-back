c0ac97cd7f6ca19ddcd770e32f7e730f7bbbc050
veremeychukden <veremeychukden@gmail.com>
2023-12-26 18:54:50 +0200

    Connect database

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const PORT = 8000;
+
+mongoose
+  .connect(
+    "mongodb+srv://veremeychukden:CNlgsNdzlWwKJenG@bike-booking.wvrw2zg.mongodb.net/?retryWrites=true&w=majority"
+  )
+  .then(() => console.log("Connected to DB successfuly"))
+  .catch((err) => console.log("DB connection error: ", err));
+
+const app = express();
+app.use(express.json());
+
+app.listen(PORT, () => console.log(`Server is running on port: ${PORT}`));
